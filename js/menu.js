ca889107804531e880b4ba75af68b0a2fa1474df
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2019-11-26 10:02:41 +0100

    Aggiunto menu mobile con JS
    Eventualmente rivederne l'estetica e pensare al noscript

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var m = document.getElementById("menu");
+  if (m.style.display === "block") {
+    m.style.display = "none";
+  } else {
+    m.style.display = "block";
+  }
+}
