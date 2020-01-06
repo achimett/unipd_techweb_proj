d20c5e3b8fc4efbf64d66d082eef9006229b1982
Biohack96 <54334833+Biohack96@users.noreply.github.com>
2020-01-06 01:31:39 +0100

    Update DB.php

--- includes/DB.php
+++ includes/DB.php
@@ -1,14 +1,35 @@
 <?php
 /* CLASSE MOCK PER DB */
-class DB {
-  public function getUser($mock = NULL) {
-    $usr = [
-      'img' => '../img/utentebianco_icon.png',
-      'name' => 'Francesco',
-      'surname' => 'De Salvador',
-    ];
-
-    return $usr;
-  }
+class DB extends mysqli{
+	
+	public function __construct($host="localhost", $user="user", $pass="password", $db="doit") 
+	{
+        parent::__construct($host, $user, $pass, $db);
+		
+		if (mysqli_connect_error()) {
+            die('Connect Error (' . mysqli_connect_errno() . ') '
+                    . mysqli_connect_error());
+        }
+	}
+	
+	public function getUser($id = NULL) 
+	{
+		$query = $this->prepare("SELECT email,nome,cognome,datanascita,cf,bio,img_path FROM `utente` WHERE id=?");
+		$query->bind_param("i", $id);
+		$query->execute();
+		$result = $query->get_result();
+		
+		/*preparo la query, la eseguo e ottengo i risultati*/
+	
+		if($result->num_rows === 0) exit('No rows'); /*check sul risultato ritornato*/
+		
+		$usr = $result->fetch_assoc(); /*traformo il risultato della query in un array associativo*/
+		
+		foreach($usr as $key => $value)
+		{echo "\n".$key."  ".$usr["$key"];} /*ciclo per il debug*/
+		
+		returng $usr;
+		
+    }
 }
 ?>
