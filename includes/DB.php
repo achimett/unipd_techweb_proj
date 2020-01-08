a9a0eaed393a1f63d888c790a4cf912abe9978a2
DSFrancesco <francesco.desalvador@gmail.com>
2020-01-08 22:10:46 +0100

    closes #26; il post viene generato dinamicamente. la funzione createPost usa le funzioni getPost getCommenti e getVolontari dell'interfaccia del il backend per costruire il post passato in GET. manca: errore sul reindirizzamento al postEdit, errore esiste sempre un post. feature: predisposto per la modifica del testo del bottone doit a seconda dei casi. controllo permessi. reindirizzamenti.

--- includes/DB.php
+++ includes/DB.php
@@ -10,5 +10,68 @@ class DB {
 
     return $usr;
   }
+
+  public function getPost($mock = NULL) {
+    $post = [
+
+      'link' => 'https://goo.gl/maps/KpxfvzkjXvCUyFcr7',
+      'chiuso' => false,
+      'partecipo' => false,
+      'img_path' => 'img/_template_foto/rifiuti3.jpg',
+      'titolo' => 'RACCOLTA RIFIUTI',
+      'id_autore' => 1,
+      'nome' => 'Paolo',
+      'cognome' => 'Rossi',
+      'nvolontari' => 6,
+      'data' => '12/02/2020',
+      'ora' => '12:00',
+      'via' => 'Via Luigi Luzzatti',
+      'provincia' => 'Padova',
+      'descrizione' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur luctus mi. Nulla id luctus lorem. Suspendisse semper in lectus sed convallis. Sed imperdiet lectus non rhoncus laoreet. Duis condimentum vel mauris facilisis sollicitudin. Mauris nec sapien ipsum. In vulputate tincidunt sapien, et porta lorem commodo a. Aenean vitae mer augue.'
+    ];
+
+    return $post;
+  }
+
+  public function getCommenti($mock = NULL) {
+    $postSocial = array();
+    $postSocial[0] = [
+      'nome' => 'Gianni',
+      'cognome' => 'Bianchi',
+      'data' => '12/02/12 - 13:12',
+      'text' => 'dksdhtfgjkghjhj uisdfh dsui',
+      'img_usr_path' => 'img/_template_foto/gianni_morandi.jpg',
+      'img_path' => 'img/_template_foto/back.jpeg',
+    ];
+    $postSocial[1] = [
+      'nome' => 'Gianni',
+      'cognome' => 'Bianchi',
+      'data' => '12/02/13 - 13:12',
+      'text' => 'dksdhfuiodf usdfh uisdfh dsui',
+      'img_usr_path' => 'img/_template_foto/gianni_morandi.jpg',
+      'img_path' => 'img/_template_foto/back.jpeg',
+    ];
+
+    return $postSocial;
+  }
+
+  public function getVolontari($mock = NULL) {
+    $postVolontari = array();
+    $postVolontari[0] = [
+      'img_path' => 'img/_template_foto/gianni_morandi.jpg',
+      'id' => '23',
+      'nome' => 'ALdo',
+      'cognome' => 'Morin',
+    ];
+    $postVolontari[1] = [
+      'id' => '23',
+      'img_path' => 'img/_template_foto/gianni_morandi.jpg',
+      'nome' => 'Francesco',
+      'cognome' => 'Decet',
+    ];
+    return $postVolontari;
+  }
+
+
 }
 ?>
