414fab40e8f7bfe89e2b1886b8e1ac376be43328
DSFrancesco <francesco.desalvador@gmail.com>
2020-01-16 10:30:04 +0100

    closes #36
    sostituiti tutti i segnaposto di body

--- index.php
+++ index.php
@@ -20,7 +20,12 @@ $scripts = file_get_contents('includes/menuScript.html'); // . file_get_contents
 // Codice HTML del content
 $content = file_get_contents('includes/index.html');
 
+
 $page_head = str_replace('<title />', "<title>$title - DOIT</title>", $page_head);
+$page_head = str_replace('<scripts />', $scripts, $page_head);
+$page_body = str_replace('<info_utente />', '', $page_body);
+$page_body = str_replace('<breadcrumb />', '', $page_body);
+$page_body = str_replace('<menu />', '', $page_body);
 $page_body = str_replace('<content />', $content, $page_body);
 
 echo $page_head . $page_body;
