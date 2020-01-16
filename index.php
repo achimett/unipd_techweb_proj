d53f774ff4728df9af72c7f7de8d3faefd9eab81
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2020-01-16 16:49:45 +0100

    Rimozione parti aggiuntive non necessarie

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-session_start();
-$_SESSION['user_id'] = 12;
-require_once('includes/DB.php');
-
-// Oggetto di accesso al database
-$db = new DB();
-
 // Titolo della pagina
 $title = 'Benvenuto';
 
@@ -13,16 +6,11 @@ $title = 'Benvenuto';
 $page_head = file_get_contents('includes/head.html');
 $page_body = file_get_contents('includes/body.html');
 
-// Concatenazione di tutti i JS da includere nell'head
-$scripts = file_get_contents('includes/menuScript.html'); // . file_get_contents(...) . ecc...;
-
-
 // Codice HTML del content
 $content = file_get_contents('includes/index.html');
 
-
 $page_head = str_replace('<title />', "<title>$title - DOIT</title>", $page_head);
-$page_head = str_replace('<scripts />', $scripts, $page_head);
+$page_head = str_replace('<scripts />', '', $page_head);
 $page_body = str_replace('<info_utente />', '', $page_body);
 $page_body = str_replace('<breadcrumb />', '', $page_body);
 $page_body = str_replace('<menu />', '', $page_body);
