1ddacbb162d7703fd1d246bbfe55887e36932ae9
Biohack96 <54334833+Biohack96@users.noreply.github.com>
2020-01-26 23:33:08 +0100

    Update validateProfilo.js

--- js/validateProfilo.js
+++ js/validateProfilo.js
@@ -13,6 +13,7 @@ function togliErrore(input) {
 
   var span = p.getElementsByTagName('span');
   if (span.length > 0) {
+	console.log(input, " " , p);
     p.removeChild(span[0]);
   }
 }
@@ -23,12 +24,20 @@ function checkData(input) {
     mostraErrore(input, "Data non valida");
     return false;
   }
+  return true;
+}
 
-function checkPass(input) {
+function checkPass(input, input2) {
   var d = /^(?=.*[0-9])(?=.*[A-Z]).{8,}$/;
-  if (d.test(input.value) == false) {
-    mostraErrore(input, "Password non valida");
+  if (input.value != input2.value) {
+    mostraErrore(input, "Password e Conferma password non coincidono");
     return false;
+  }
+  if (d.test(input.value) == false) {
+    mostraErrore(input, "Password non valida : la password deve essere di almeno 8 caratteri e con almeno un alettere maiuscola ed un numero");
+    return false;
+  }
+ return true;
   }
   
 function checkMail(input) {
@@ -37,40 +46,64 @@ function checkMail(input) {
     mostraErrore(input, "Mail non valida");
     return false;
   }
+  return true;
+}
   
   
 function checkTel(input) {
   var d = /^[0-9]{7,12}$/;
   if (d.test(input.value) == false) {
-    mostraErrore(input, "Numero di telefono non valida");
+    mostraErrore(input, "Numero di telefono non valido");
     return false;
   }
+  return true;
+}
  
  function checkNome(input) {
   var d = /^[a-zA-Z ]{2,30}$/;
   if (d.test(input.value) == false) {
-    mostraErrore(input, "Nome non valida");
+    mostraErrore(input, "Nome non valido");
     return false;
   }
+  return true;
+ }
+  
+  function checkBio(input) {
+  if (input.value.length > 65535) {
+    mostraErrore(input, "Hai inserito troppi caratteri");
+    return false;
+
+  } else if (input.value.length <= 0) {
+    mostraErrore(input, "Biografia vuoto, raccontaci qualcosa di te :)");
+    return false;
+  }
+
+  togliErrore(input);
+  return true;
+}
   
   function checkCognome(input) {
   var d = /^[a-zA-Z ]{2,30}$/;
   if (d.test(input.value) == false) {
-    mostraErrore(input, "Cognome non valida");
+    mostraErrore(input, "Nome non valido");
     return false;
+  }
+  return true;
   }
   
   function checkCF(input) {
   var d = /^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$/;
   if (d.test(input.value) == false) {
-    mostraErrore(input, "Cognome non valida");
+    mostraErrore(input, "Codice fiscale non valido");
     return false;
+  }
+  return true;
   }
   
   function checkImg() {
-  var img = document.getElementById("postEdit_social_sfoglia"); //// cambaire 
+  var img = document.getElementById("registrazione_foto");
 
-  var x = document.getElementById("postEdit_social_sfoglia").value;
+  var x = document.getElementById("registrazione_foto").value;
   if (x.split('.').pop() != "png" && x.split('.').pop() != "jpg" && x.split('.').pop() != "jpeg") {
     mostraErrore(img, "Selezionare un file in formato png o jpeg");
     return false;
@@ -82,12 +115,15 @@ function checkTel(input) {
 
 
 function validateProfilo() {   /////cambaire id
-  var titolo = document.getElementById('postEdit_core_titolo_input');
-  var descrizione = document.getElementById("postEdit_social_textarea");
-  var data = document.getElementById("data");
-  var ora = document.getElementById("ora");
-  var luogo = document.getElementById("luogo");
-  var provincia = document.getElementById("provincia");
+  var nome = document.getElementById('registrazione_nome');
+  var cognome = document.getElementById("registrazione_cognome");
+  var data = document.getElementById("registrazione_data_di_nascita");
+  var cf = document.getElementById("registrazione_codice_fiscale");
+  var mail = document.getElementById("registrazione_email");
+  var cell = document.getElementById("registrazione_telefono");
+  var pass = document.getElementById("registrazione_password");
+  var c_pass = document.getElementById("registrazione_conferma_password");
+  var bio = document.getElementById("registrazione_biografia");
 
-  return (checkTitolo(titolo) & checkDescrizione(descrizione) & checkData(data) & checkOra(ora) & checkLuogo(luogo) & checkProvincia(provincia)) != 0;
+  return (checkNome(nome) & checkCognome(cognome) & checkData(data) & checkCF(cf) & checkTel(cell) & checkImg() & checkBio(bio)  & checkPass(pass,c_pass) & checkMail(mail)) != 0;
 }
