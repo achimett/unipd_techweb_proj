72bfe960d4ed492a34113eb7e923cee99f894735
DSFrancesco <francesco.desalvador@gmail.com>
2020-01-27 01:55:47 +0100

    about css e help

---
+++ help.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+require_once('includes/DB.php');
+require_once('includes/createInfoUtente.php');
+require_once('includes/createMenu.php');
+
+// Oggetto di accesso al database
+$db = new DB();
+
+// Titolo della pagina
+$title = 'Chi Siamo';
+
+// Contengono l'HTML dei tag <head> e <body> che verranno stampati
+$page_head = file_get_contents('includes/head.html');
+$page_body = file_get_contents('includes/body.html');
+
+// Concatenazione di tutti i JS da includere nell'head
+$scripts = file_get_contents('includes/scriptMenu.html'); // . file_get_contents(...) . ecc...;
+
+// Contiene lo snippet di codice per visualizzare l'utente loggato in alto a sinistra
+$info_utente = createInfoUtente($db);
+
+// Codice HTML del menu
+$menu = createMenu(true, true, true, true, false, true);
+
+// Codice HTML del breadcrumb
+$breadcrumb = '<p id="breadcrumb">Aiuto</p>';
+
+// Codice HTML del content
+$content = file_get_contents('includes/contentHelp.html');
+
+// Rimpiazzo dei segnaposto sull'intera pagina
+$page_head = str_replace('<title />', "<title>$title - DOIT</title>", $page_head);
+$page_head = str_replace('<scripts />', $scripts, $page_head);
+$page_body = str_replace('<info_utente />', $info_utente, $page_body);
+$page_body = str_replace('<breadcrumb />', $breadcrumb, $page_body);
+$page_body = str_replace('<menu />', $menu, $page_body);
+$page_body = str_replace('<content />', $content, $page_body);
+
+echo $page_head . $page_body;
+?>
