f38e09eb9d337d6d4aa8909b8ecbdfb394621cc8
Biohack96 <54334833+Biohack96@users.noreply.github.com>
2020-01-27 01:30:58 +0100

    Update DB.php

--- includes/DB.php
+++ includes/DB.php
@@ -16,9 +16,8 @@ class DB extends mysqli{
         parent::__construct($host, $user, $pass, $db);
 
 		if (mysqli_connect_error()) {
-            die('Connect Error (' . mysqli_connect_errno() . ') '
-                    . mysqli_connect_error());
-        }
+           	 die();
+        	}
 	}
 
 
