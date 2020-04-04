01f12ca12f6d18c410857d9d7dc187b69ee37a17
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2020-04-04 20:37:00 +0200

    Corretto script di reset
    - Non ci sono più partecipazioni ripetute
    - echo in vari punti dello script per debugging

--- reset/script.php
+++ reset/script.php
@@ -1,7 +1,14 @@
 <?php
+echo "Script avviato <br>";
+echo "Pulizia cache <br>";
+opcache_reset();
+clearstatcache();
+echo "Cache pulita <br>";
+
 session_start();
 require_once('../includes/DB.php');
 
+echo "Creazione connessione al database<br>";
 $db = new DB();
 
 $email = file("email.txt");
@@ -22,6 +29,8 @@ $db->addUserUser();
 
 for($i = 0; $i < $tot; $i++) {
 
+  echo "Creazione nuovo profilo e post <br>";
+
   $r["email"] = rand(0, $lim);
 
   $r["password"] = rand(0, $lim);
@@ -39,11 +48,7 @@ for($i = 0; $i < $tot; $i++) {
   $r["descrizione"] = rand(0, $lim);
 
 
-
   $cf = '';
-
-
-
   $cf .= $char[rand(0, 25)];
   $cf .= $char[rand(0, 25)];
   $cf .= $char[rand(0, 25)];
@@ -60,15 +65,24 @@ for($i = 0; $i < $tot; $i++) {
 
 
   $x = $db->setProfilo(NULL, trim($email[$r["email"]]), trim($password[$r["password"]]), trim($password[$r["password"]]), trim($nome[$r["nome"]]), trim($nome[$r["cognome"]]),
-   trim($r['birthday']), trim($cf), trim($text[$r['bio']]), NULL, trim($r['tel']));
+  trim($r['birthday']), trim($cf), trim($text[$r['bio']]), NULL, trim($r['tel']));
 
-    $y = $db->setPost(NULL, $r['titolo'], $x, $r['data'], $r['ora'], $text[$r['descrizione']], NULL, $luogo[$r['luogo']], $provincia[$r['provincia']]);
+  echo "Creato profilo $x <br>";
+
+  $y = $db->setPost(NULL, $r['titolo'], $x, $r['data'], $r['ora'], $text[$r['descrizione']], NULL, $luogo[$r['luogo']], $provincia[$r['provincia']]);
+
+  echo "Creato post $y <br>";
 
 
 $c = rand(50, 100);
 for($k = 0; $k < $c; $k++) {
 
-    $o = $db->partecipa(rand(0, $i), $x);
+  $p = rand(1, $i);
+  $res = $db->isAutore($p, $x);
+  if($res === false || is_null($res)) {
+    echo "Aggiunta partecipazione utente $x a post $p <br>";
+    $o = $db->partecipa($p, $x);
+  }
 $z = $db->newCommento(rand(0, $i), rand(0, $tot), $text[$r['bio']], NULL);
 }
 
@@ -79,6 +93,6 @@ unset($_SESSION['user_id']);
 $db->setPost(NULL, "Raccolta rifiuti", 1, "11/11/2019", "12:23", "descrizione", NULL, "Lungo piovego", "Padova (PD)");
 
 
-echo "Reset e ripopolazione database eseguita"
+echo "Reset e ripopolazione database eseguita! Ora è possibile ritornare a DOIT."
 
 ?>
