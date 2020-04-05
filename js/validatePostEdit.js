871df343f0b78c27405ae916a515853a3cda2bf4
Daniele <danielebresciani3a@gmail.com>
2020-04-05 19:25:19 +0200

    Correzione data/ora/descrizione
    CONTENTPOSTEDIT.HTML
    - Viene mostrato nella label il numero massimo di caratteri inseribili.
    
    VALIDATEPOSTEDIT.JS
    - L'errore restituito nel caso in cui l'utente sbagliasse data, indica il formato in cui la data deve essere inserita.

--- js/validatePostEdit.js
+++ js/validatePostEdit.js
@@ -1,7 +1,7 @@
 function checkData(input) {
   var d = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:19|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:19|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:19|[2-9]\d)?\d{2})$/ ;
   if (d.test(input.value) == false) {
-    mostraErrore(input, "Data non valida");
+    mostraErrore(input, "Data non valida, formato data: dd/mm/aaaa");
     return false;
   }
   togliErrore(input);
@@ -11,7 +11,7 @@ function checkData(input) {
 function checkOra(input) {
   var d = new RegExp('^(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]$');
   if (d.test(input.value) == false) {
-    mostraErrore(input, "Orario non valido, rispettare il formato HH:MM");
+    mostraErrore(input, "Orario non valido, formato ora: hh:mm");
     return false;
   }
 
